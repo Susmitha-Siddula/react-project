d0b5d64213d5bbfcb15371063da06f59c84ca206
Susmitha Siddula <susmithasiddula@gmail.com>
2023-09-07 17:10:37 -0500

    epidose-02

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", { id: "heading1" }, "This is h1"),
-  React.createElement("h2", { id: "heading2" }, "This is h2"),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is Susmitha"),
+    React.createElement("h2", {}, "React project"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is h1"),
+    React.createElement("h2", {}, "This is h2"),
+  ]),
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ README.md
@@ -0,0 +1,21 @@
+React Project
+
+#parcel features
+
+- Dev build
+- Local server
+- HMR (Hot Module replacement)
+- File Watching Algorithm, written in c++
+- Caching - Faster builds
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Code Splitting
+- Consistent Hashing
+- Differential Bundling
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
